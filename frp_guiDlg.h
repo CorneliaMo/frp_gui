0b3fde739e6e3d74fb5776b6f2f6885612c31544
CorneliaMo <moweihao2@163.com>
2024-10-06 20:30:57 +0800

    build frame

---
+++ frp_guiDlg.h
@@ -0,0 +1,46 @@
+﻿
+// frp_guiDlg.h: 头文件
+//
+
+#pragma once
+
+
+// CfrpguiDlg 对话框
+class CfrpguiDlg : public CDialogEx
+{
+// 构造
+public:
+	CfrpguiDlg(CWnd* pParent = nullptr);	// 标准构造函数
+
+// 对话框数据
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_FRP_GUI_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV 支持
+
+
+// 实现
+protected:
+	HICON m_hIcon;
+
+	// 生成的消息映射函数
+	virtual BOOL OnInitDialog();
+	afx_msg void OnSysCommand(UINT nID, LPARAM lParam);
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+public:
+	afx_msg void OnBnClickedMfcbutton1();
+	afx_msg void OnBnClickedMfcbutton2();
+	CMFCButton m_startButton;
+	afx_msg void OnBnClickedStartbutton();
+	afx_msg void OnBnClickedRestartbutton();
+	afx_msg void OnBnClickedAddbutton();
+	afx_msg void OnBnClickedEditbutton();
+	afx_msg void OnBnClickedDeletebutton();
+	CListCtrl m_list;
+	CMFCButton m_restartButton;
+	CStatic m_statusText;
+};
---
+++ resource.h
@@ -0,0 +1,28 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 frpgui.rc 使用
+//
+#define IDM_ABOUTBOX                    0x0010
+#define IDD_ABOUTBOX                    100
+#define IDS_ABOUTBOX                    101
+#define IDD_FRP_GUI_DIALOG              102
+#define IDR_MAINFRAME                   128
+#define IDC_STARTBUTTON                 1000
+#define IDC_RESTARTBUTTON               1001
+#define IDC_ADDBUTTON                   1002
+#define IDC_EDITBUTTON                  1003
+#define IDC_DELETEBUTTON                1004
+#define IDC_LIST2                       1005
+#define IDC_LIST                        1005
+#define IDC_STATUS_TEXT                 1006
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        131
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1007
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
